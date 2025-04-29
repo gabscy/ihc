34de4d3eccd316db05529d608cefacb9e09d454a
gabrielyin <2021gabrielyin@gmail.com>
2025-04-29 10:14:15 -0300

    paginas view

---
+++ src/app/login/page.tsx
@@ -0,0 +1,89 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+export default function Login() {
+  return (
+    <>
+      <header>
+        <div className="flex justify-between items-center py-2 px-8 border-b">
+          <Label className="text-2xl">Guarda Roupa Virtual</Label>
+
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Button variant={"secondary"}>
+                  Login
+                </Button>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Button>
+                  Cadastrar
+                </Button>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </header>
+
+      <main className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className={cn("flex flex-col gap-6")}>
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">Login</CardTitle>
+              <CardDescription>
+                Fazer login
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid gap-6">
+                  <div className="grid gap-6">
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                        <a
+                          href="#"
+                          className="ml-auto text-sm underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                      <Input id="password" type="password" required />
+                    </div>
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                  </div>
+                  <div className="text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <a href="#" className="underline underline-offset-4">
+                      Sign up
+                    </a>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+          <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+            By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+            and <a href="#">Privacy Policy</a>.
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,94 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { Plus, Search } from "lucide-react"
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      Home      
-    </div>
+    <>
+      <header>
+        <div className="flex justify-between items-center py-2 px-8 border-b">
+          <Label className="text-2xl">Guarda Roupa Virtual</Label>
+
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Métricas
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Minha Conta
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Combincação
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Button variant={"secondary"}>
+                  Login
+                </Button>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <Button>
+                  Cadastrar
+                </Button>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </header>
+
+      <main className="max-w-6xl mx-auto py-8">
+        <div className="flex gap-4">
+          <div className="relative flex-1">
+            <Label htmlFor="search" className="sr-only">
+              Search
+            </Label>
+            <Input
+              id="search"
+              placeholder="Search the docs..."
+              className="pl-8"
+            />
+            <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+          </div>
+
+          <Button
+            className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
+          >
+            <Plus />
+            <span>Cadastrar Roupa</span>
+          </Button>
+        </div>
+
+        <section className="grid grid-cols-3 pt-4"> 
+          <Card>
+            <CardContent>
+              <div className="relative w-full h-[240px] mb-4">
+                <Image alt="imagem" src="/" fill className="bg-gray-200" />
+              </div>
+              <CardTitle>Nome da roupa</CardTitle>
+              <CardDescription>Breve descrição</CardDescription>
+            </CardContent>
+          </Card>
+        </section>
+      </main>
+    </>
   );
 }
